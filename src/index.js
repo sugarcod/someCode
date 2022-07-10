c6414d02c96f39044d89856dbdff74b87cb0ab09
sugarcod <102928648+sugarcod@users.noreply.github.com>
2022-07-10 18:16:23 +0300

    feat: change schema

--- src/index.js
+++ src/index.js
@@ -171,58 +171,47 @@ document.getElementById("app").innerHTML = `
 //     .replace(/0/g, "");
 
 // Expected: 'E', instead got: 'T'
-let res = [];
-let addEnd = (num, arr) => {
-  arr.push(num);
-};
+// let res = [];
+// let addEnd = (num, arr) => {
+//   arr.push(num);
+// };
 
-let snail = function (a) {
-  if (Array.isArray(a)) {
-    if (a.length == 0) {
-      return [];
-    }
-  }
-  let down = true;
-  let inno = 0;
-  function go(index) {
-    for (let i = index; i < a.length; i++) {
-      const element = a[i];
-      down && i == 0 ? addEnd(element, res) : null;
-      down && (i > 0) & (i < a.length - 1)
-        ? addEnd(element[a.length - 1], res)
-        : null;
-      if (down && i == a.length - 1) {
-        for (let i = element.length - 1; i >= 0; i--) {
-          addEnd(element[i], res);
-        }
-        down = false;
-        inno++;
-      }
-    }
-    if (!down) {
-      for (let i = a.length - 2; i >= 1; i--) {
-        addEnd(a[i][inno], res);
-      }
-    }
-    console.log("end");
-  }
-  if (inno < a.length) {
-    console.log("start");
-    go(inno);
-  }
-  return res;
-};
-
-console.log(
-  snail([
-    [1, 2, 3, 4, 5, 6],
-    [20, 21, 22, 23, 24, 7],
-    [19, 32, 33, 34, 25, 8],
-    [18, 31, 36, 35, 26, 9],
-    [17, 30, 29, 28, 27, 10],
-    [16, 15, 14, 13, 12, 11]
-  ])
-);
+// let snail = function (a) {
+//   if (Array.isArray(a)) {
+//     if (a.length == 0) {
+//       return [];
+//     }
+//   }
+//   let down = true;
+//   let inno = 0;
+//   function go(index) {
+//     for (let i = index; i < a.length; i++) {
+//       const element = a[i];
+//       down && i == 0 ? addEnd(element, res) : null;
+//       down && (i > 0) & (i < a.length - 1)
+//         ? addEnd(element[a.length - 1], res)
+//         : null;
+//       if (down && i == a.length - 1) {
+//         for (let i = element.length - 1; i >= 0; i--) {
+//           addEnd(element[i], res);
+//         }
+//         down = false;
+//         inno++;
+//       }
+//     }
+//     if (!down) {
+//       for (let i = a.length - 2; i >= 1; i--) {
+//         addEnd(a[i][inno], res);
+//       }
+//     }
+//     console.log("end");
+//   }
+//   if (inno < a.length) {
+//     console.log("start");
+//     go(inno);
+//   }
+//   return res;
+// };
 
 //https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
 
@@ -237,3 +226,51 @@ console.log(
 //       [17, 30, 29, 28, 27, 10],
 //       [16, 15, 14, 13, 12, 11]
 //     ]),
+
+let snail = (arr) => {
+  let res = [];
+  let row = 1;
+  let count = arr.length;
+
+  for (let i = 0; i < count; i++) {
+    for (let k = 0; k < arr.length; k++) {
+      let yes = true;
+      //console.log(arr[k], k, i, "arr[k]");
+
+      if (k == i) {
+        if (k == 0) {
+          res.push(arr[k].slice(k, arr.length));
+        } else {
+          res.push(arr[k].slice(k, arr.length - k));
+        }
+      }
+
+      if (k == arr.length - row) {
+        console.log(
+          arr[k].slice(row, arr.length - row),
+          row,
+          "k == arr.length"
+        );
+
+        res.push(arr[k].slice(k, arr.length));
+      }
+      if (k !== i && k !== arr.length - row) {
+        res.push(arr[k][arr.length - row]);
+      }
+    }
+    row++;
+  }
+
+  return res;
+};
+
+console.log(
+  snail([
+    [1, 2, 3, 4, 5, 6],
+    [20, 21, 22, 23, 24, 7],
+    [19, 32, 33, 34, 25, 8],
+    [18, 31, 36, 35, 26, 9],
+    [17, 30, 29, 28, 27, 10],
+    [16, 15, 14, 13, 12, 11]
+  ])
+);
